a323933f383c7666bf9f4cd49abbedb3379a244e
Andrei Barcovschi <abarcovschi@gmail.com>
2023-05-12 22:00:51 +0100

    working transcribe_speech.py in nvcr.io/nvidia/pytorch:23.03-py3 container with NeMo branch r1.18.0 installed

---
+++ run_inference_batch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+declare -a arr_dataset=(
+	"/workspace/datasets/myst_w2v2_asr/test_manifest.json"
+	"/workspace/datasets/cmu_hf/test_manifest.json"
+	"/workspace/datasets/pfe_16khz/test_manifest.json")
+
+declare -a arr_model=(
+	"/workspace/nemo/non-finetuned/models/stt_en_conformer_transducer_small.nemo"
+	"/workspace/nemo/non-finetuned/models/stt_en_conformer_transducer_medium.nemo"
+	"/workspace/nemo/non-finetuned/models/stt_en_conformer_transducer_large.nemo"
+    "/workspace/nemo/non-finetuned/models/stt_en_conformer_transducer_xlarge.nemo"
+    "/workspace/nemo/non-finetuned/models/stt_en_conformer_transducer_xxlarge.nemo")
+
+arr_model_len=${#arr_model[@]}
+arr_dataset_len=${#arr_dataset[@]}
+
+# use for loop to read all values and indexes
+ix=0
+for (( i=0; i<${arr_model_len}; i++ ));
+do
+	for (( j=0; j<${arr_dataset_len}; j++ ));
+	do
+        echo "${ix}) ${arr_model[$i]} ${arr_dataset[$j]}" >> run_inference_batch.log
+        echo "${arr_model[$i]} ${arr_dataset[$j]}"  >> whisper_out4.log
+        python3 transcribe_speech.py model_path="${arr_model[$i]}" dataset_manifest="${arr_dataset[$j]}" output_filename="temp.json" | grep -e 'wer' >> run_inference_batch.log
+        ((ix++))
+	done
+done
