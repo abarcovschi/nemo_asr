565ca2dfd19a9afa9d83ef3ebde78b61a1e570bc
Andrei Barcovschi <abarcovschi@gmail.com>
2024-03-20 16:09:20 +0000

    update manifest preparation file

--- scripts/prepare_manifest_Wearable_Audio_Diarized.py
+++ scripts/prepare_manifest_Wearable_Audio_Diarized.py
@@ -2,18 +2,23 @@ import os
 import json
 import librosa
 
-"""Build manifest.json file for transcribing a diarization tool's output.
+"""
+Build manifest.json file for transcribing a diarization tool's output.
 (all diarization tools used in the speech-augmentation pipeline output a common format, so this script will work for each tool).
 Diarization options described here: https://github.com/C3Imaging/speech-augmentation#speaker-diarization
 """
 
-# Function to build a manifest
+
 def build_manifest(src_data_path, manifest_path):
+    """
+    Builds a manifest JSON file, which will list the wav files that need transcribing.
+    For each wav file, add a line in the following format: {"audio_filepath": /path/to/audio.wav, "duration": time in secs}
+    """
     with open(manifest_path, 'w') as fout:
-        for dirpath, subdirs, filenames in os.walk(src_data_path, topdown=False):
+        for dirpath, _, filenames in os.walk(src_data_path, topdown=False):
             for filename in filenames:
                 if filename.endswith(".wav"):
-                    # Write the metadata to the manifest
+                    # Write the metadata to the manifest.
                     metadata = {
                         "audio_filepath": os.path.join(dirpath, filename),
                         "duration": librosa.core.get_duration(filename=os.path.join(dirpath, filename)),
@@ -23,12 +28,9 @@ def build_manifest(src_data_path, manifest_path):
 
                 
 if __name__ == "__main__":
-    # Building Manifest
     data_dir_root = '/workspace/datasets/LibriTTS_test_whisper'
-    print("******")
-    # creating manifest
-    manifest_path = os.path.join(data_dir_root, 'manifest.json') # to be created
-    # if manifest does not yet exist
+    manifest_path = os.path.join(data_dir_root, 'manifest.json') # to be created.
+    # if manifest does not yet exist.
     if not os.path.isfile(manifest_path):
         build_manifest(data_dir_root, manifest_path)
         print(f"{manifest_path} manifest file created.")
